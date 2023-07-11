4c9cc46589a00426401044ffec3c0dfe1035ec08
euntae-bae <euntae471@gmail.com>
2023-07-11 13:16:36 +0900

    FNDCounter is updated

--- week2/src/FNDCounter/FNDCounter.v
+++ week2/src/FNDCounter/FNDCounter.v
@@ -16,24 +16,27 @@ module RefreshCounter(sysclk, rstn, ovf);
             ovf <= 0;
         end
         else begin
-            if (cnt == refreshRate)
+            if (cnt == refreshRate) begin
                 ovf <= 1;
-            else
+                cnt <= 0;
+            end
+            else begin
                 ovf <= 0;
-            cnt <= cnt + 1;
+                cnt <= cnt + 1;
+            end
         end
     end
 endmodule
 
-//100ms=100*1000us=100*1000*1000ns=10^8ns=10^7*10ns => 24비트로 표현 가능
-module Counter24(sysclk, rstn, ovf);
+module Counter24(sysclk, rstn, clksel, ovf);
     input sysclk;
     input rstn;
+    input clksel;
     output reg ovf;
 
     reg [23:0] cnt;
     //parameter rate = 10000000;
-    parameter rate = 100000;
+    parameter rate = 1000000;
 
     always @(posedge sysclk or negedge rstn) begin
         if (!rstn) begin
@@ -41,89 +44,110 @@ module Counter24(sysclk, rstn, ovf);
             ovf <= 0;
         end
         else begin
-            if (cnt == rate)
-                ovf <= 1;
-            else
-                ovf <= 0;
-            cnt <= cnt + 1;
+            if (clksel) begin
+                if (cnt == (rate / 2)) begin
+                    ovf <= 1;
+                    cnt <= 0;
+                end
+                else begin
+                    ovf <= 0;
+                    cnt <= cnt + 1;
+                end
+            end
+            else begin
+                if (cnt == rate) begin
+                    ovf <= 1;
+                    cnt <= 0;
+                end
+                else begin
+                    ovf <= 0;
+                    cnt <= cnt + 1;
+                end
+            end
         end
     end
 endmodule
 
 // Top module
-module FNDCounter(clk, rstn, seg, an);
+module FNDCounter(clk, rstn, clksel, seg, an);
     input clk;
     input rstn;
+    input clksel;
     output [6:0] seg;
     output reg [7:0] an;
-    reg [3:0] bcdArr [7:0]; // 8개 FND의 BCD값
-    reg bcdCarry;
-    wire [3:0] bcd;
-    reg [2:0] fndsel;       // 현재 출력할 FND 선택
-
-    integer i;
+    reg [31:0] fndcnt;
+    reg [2:0]  fndsel;       // 현재 출력할 FND 선택
+    reg [3:0] bcd;
 
     wire fndclk;
     wire refreshclk;
 
-    Counter24 fctr(clk, rstn, fndclk);
+    Counter24 fctr(clk, rstn, clksel, fndclk);
     RefreshCounter rctr(clk, rstn, refreshclk);
     BCDDecoder dec(bcd, seg);
 
-    assign bcd = bcdArr[fndsel];
+    //assign bcd = bcdArr[fndsel];
 
     always @(*) begin
         case (fndsel) 
-            3'b000: an = 8'b00000001;
-            3'b001: an = 8'b00000010;
-            3'b010: an = 8'b00000100;
-            3'b011: an = 8'b00001000;
-            3'b100: an = 8'b00010000;
-            3'b101: an = 8'b00100000;
-            3'b110: an = 8'b01000000;
-            3'b111: an = 8'b10000000;
-            default: an = 8'b00000001;
+            3'b000: begin
+                an = 8'b1111_1110;
+                bcd = fndcnt % 10;
+            end
+            3'b001: begin
+                an = 8'b1111_1101;
+                bcd = (fndcnt / 10) % 10;
+            end
+            3'b010: begin
+                an = 8'b1111_1011;
+                bcd = (fndcnt / 100) % 10;
+            end
+            3'b011: begin
+                an = 8'b1111_0111;
+                bcd = (fndcnt / 1000) % 10;
+            end
+            3'b100: begin
+                an = 8'b1110_1111;
+                bcd = (fndcnt / 10000) % 10;
+            end
+            3'b101: begin
+                an = 8'b1101_1111;
+                bcd = (fndcnt / 100000) % 10;
+            end
+            3'b110: begin
+                an = 8'b1011_1111;
+                bcd = (fndcnt / 1000000) % 10;
+            end
+            3'b111: begin
+                an = 8'b0111_1111;
+                bcd = (fndcnt / 10000000) % 10;
+            end
+            default: begin
+                an = 8'b1111_1110;
+                bcd = fndcnt % 10;
+            end
         endcase
     end
 
-    // BCD counter value update
-    always @(posedge fndclk or negedge rstn) begin
+    // FND counter update
+    always @(posedge clk or negedge rstn) begin
         if (!rstn) begin
-            for (i = 0; i < 8; i = i + 1) begin
-                bcdArr[i] = 4'b0000;
-            end
+            fndcnt <= 0;
         end
         else begin
-            if (bcdArr[0] == 4'b1001) begin
-                bcdArr[0] = 4'b0000;
-                bcdCarry = 1;
-                for (i = 1; i < 8; i = i + 1) begin
-                    if (bcdCarry == 1) begin
-                        if (bcdArr[i] == 4'b1001) begin
-                            bcdArr[i] = 4'b0000;
-                            bcdCarry = 1;
-                        end
-                        else begin
-                            bcdArr[i] = bcdArr[i] + 1;
-                            bcdCarry = 0;
-                        end
-                    end
-                end
-            end
-            else begin
-                bcdArr[0] = bcdArr[0] + 1;
-                bcdCarry = 0;
-            end
+            if (fndclk)
+                fndcnt <= fndcnt + 1;
         end
     end
 
     // FND select update
-    always @(posedge refreshclk or negedge rstn) begin
+    always @(posedge clk or negedge rstn) begin
         if (!rstn) begin
             fndsel <= 0;
         end
         else begin
-            fndsel <= fndsel + 1;
+            if (refreshclk)
+                fndsel <= fndsel + 1;
         end
     end
 endmodule
